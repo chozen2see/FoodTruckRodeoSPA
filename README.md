27f78e2b5f458ba29c984ab90f9771c8ff409104
Chozen <chozen2see@augusts-mbp.home>
2020-07-31 16:05:33 -0400

    Major Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# DatingAppSPA
+# FoodTruckRodeoSPA
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DatingApp-SPA'`, () => {
+  it(`should have as title 'FoodTruckRodeo-SPA'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DatingApp-SPA');
+    expect(app.title).toEqual('FoodTruckRodeo-SPA');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DatingApp-SPA app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'FoodTruckRodeo-SPA app is running!'
+    );
   });
 });
