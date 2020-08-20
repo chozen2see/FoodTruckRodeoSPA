ad1df34eed0c706c75f3cab68186849f45c9deee
Chozen <chozen2see@augusts-mbp.home>
2020-08-19 20:23:26 -0400

    finished shopping cart

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { Router } from '@angular/router';
+import { CartService } from '../_services/cart.service';
 
 @Component({
   selector: 'app-nav',
@@ -19,21 +20,42 @@ export class NavComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService,
+    private cartService: CartService,
     // for using routing in code
     private router: Router
   ) {}
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
     this.uniqueName = this.uniqueNameFromApp;
-    this.userId = 7;
+    this.userId = 0;
   }
 
+  // tslint:disable-next-line: typedef
   login() {
     this.authService.login(this.model).subscribe(
       (next) => {
         this.alertify.success('Logged in successfully');
         this.uniqueName = this.authService.decodedToken.unique_name;
+        this.userId = parseInt(this.authService.decodedToken.nameid, 10);
         localStorage.setItem('nid', this.authService.decodedToken.nameid);
+
+        // check to see if user has a cart saved
+        const cartId = parseInt(localStorage.getItem('cid'), 10) || 0;
+
+        // if not create one for this food truck
+        if (cartId === undefined || isNaN(cartId) || cartId === 0) {
+          const foodTruckId = parseInt(localStorage.getItem('fid'), 10);
+          this.cartService.getCart(cartId, foodTruckId, this.userId)
+          .subscribe(
+            (cart) => {
+              localStorage.setItem('cid', cart.id.toString());
+            },
+            (error) => {
+              this.alertify.error(error); // 'Failed to log in.'
+            }
+          );
+        }
       },
       (error) => {
         this.alertify.error(error); // 'Failed to log in.'
@@ -41,13 +63,16 @@ export class NavComponent implements OnInit {
     );
   }
 
+  // tslint:disable-next-line: typedef
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('nid');
+    localStorage.removeItem('cid');
     this.router.navigate(['/']);
     this.alertify.message('Logged out.');
   }
 
+  // tslint:disable-next-line: typedef
   loggedIn() {
     return this.authService.loggedIn();
   }
